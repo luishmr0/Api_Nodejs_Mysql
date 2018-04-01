e6c611f93aed0cee432a42c27bf0c40152416cab
Doo <kde1690@gmail.com>
2018-04-01 08:45:42 -0500

    body-parser

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const cors  = require('cors')
 
 app.use(morgan('dev'))
+
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json())
 app.use(cors())
 
